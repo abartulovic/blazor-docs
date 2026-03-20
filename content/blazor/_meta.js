b7881c098a2ec689abde17e10abeebb05244acd1
abartulovic <ante.bartulovic2@skole.hr>
2026-03-20 12:24:11 +0100

    Nova imena mdx fileova

--- content/blazor/_meta.js
+++ content/blazor/_meta.js
@@ -1,8 +1,87 @@
 export default {
-  index: {
-    title: "Početna",
+  index: "Početna",
+
+  Lekcija01: {
+    title: "1. Uvod u Blazor",
     theme: {
-      toc: false,
-    },
+      toc: false
+    }
   },
-};
+
+  Lekcija02: {
+    title: "2. Osnovne komponente",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija03: {
+    title: "3. Routing i navigacija",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija04: {
+    title: "4. Layout i organizacija stranica",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija05: {
+    title: "5. CRUD i baza podataka",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija06: {
+    title: "6. Rad s formama",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija07: {
+    title: "7. Pretraga, filtriranje i sortiranje",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija08: {
+    title: "8. Validacija unosa",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija09: {
+    title: "9. Rad s povezanim podacima",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija10: {
+    title: "10. Napredniji prikaz podataka",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija11: {
+    title: "11. Medicinske bilješke i evidencija tretmana",
+    theme: {
+      toc: false
+    }
+  },
+
+  Lekcija12: {
+    title: "12. Modul udomljenja",
+    theme: {
+      toc: false
+    }
+  }
+}
